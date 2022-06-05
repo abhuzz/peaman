53a7bef0928b9b2dc1ccae5a9e8828d9beb5f288
Abhay Korat <abhaykorat89@gmail.com>
2022-06-05 11:14:28 +0530

    - update listion functions

--- lib/services/messaging_services/firebase_messaging_provider.dart
+++ lib/services/messaging_services/firebase_messaging_provider.dart
@@ -1,15 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:device_info/device_info.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:peaman/peaman.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
 
 class FirebaseMessagingProvider {
   static final _firebaseMessaging = FirebaseMessaging.instance;
@@ -19,17 +13,11 @@ class FirebaseMessagingProvider {
     required final String uid,
   }) async {
     await FirebaseMessaging.instance.setAutoInitEnabled(true);
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
     try {
       final _deviceInfo = DeviceInfoPlugin();
       String? _docId;
       String? _platForm;
 
-      var file;
-      var bigPictureStyleInformation;
-      List<IOSNotificationAttachment> newattachments=[];
-      String? avatar;
 
       if (Platform.isAndroid) {
         final _androidInfo = await _deviceInfo.androidInfo;
@@ -46,67 +34,6 @@ class FirebaseMessagingProvider {
         }
         _platForm = 'ios';
       }
-
-      FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-        print("onmessage.listen ontap");
-
-        RemoteNotification? notification = message.notification;
-        // AndroidNotification? android = message.notification?.android;
-        AppleNotification? appleNot = message.notification?.apple;
-        avatar = appleNot!.imageUrl;
-
-        print(message.data);
-        print("getinitiaonMessage.listen ontap");
-
-        if (Platform.isAndroid) {
-
-        } else if (Platform.isIOS) {
-          if (avatar != null && avatar!.isNotEmpty) {
-            final extension = p.extension(avatar!);
-            final picturePath = await saveImage(Image.network(avatar!),extension);
-            newattachments.add(IOSNotificationAttachment('${picturePath}'));
-          }
-        }
-
-        if (notification != null) {
-          print("message from ${message.notification!.body}");
-          print("message from ${message.notification!.title}");
-          print("message from ${message.data}");
-
-          var platformChannelSpecificsAndroid = new AndroidNotificationDetails(
-              'your_channel_id', 'your channel name',
-              channelDescription: 'your channel description',
-              icon: 'logo_blue',
-              color: Colors.indigo,
-              playSound: false,
-              enableVibration: false,
-              importance: Importance.high,
-              styleInformation: avatar != null && avatar!.isNotEmpty
-                  ? bigPictureStyleInformation
-                  : null,
-              largeIcon: avatar != null && avatar!.isNotEmpty ? file : null,
-              priority: Priority.high);
-
-          // @formatter:on
-          var platformChannelSpecificsIos =
-          new IOSNotificationDetails(
-              presentSound: true,
-              presentAlert: true,
-              presentBadge: true,
-              attachments: newattachments
-          );
-          var platformChannelSpecifics = new NotificationDetails(
-              android: platformChannelSpecificsAndroid,
-              iOS: platformChannelSpecificsIos);
-
-          flutterLocalNotificationsPlugin.show(
-              1,
-              notification.title,
-              notification.body,
-              platformChannelSpecifics,
-          );
-        }
-      });
 
       final _token = await _firebaseMessaging.getToken();
       final _deviceRef = PeamanReferenceHelper.devicesCol(uid: uid).doc(_docId);
@@ -135,73 +62,8 @@ class FirebaseMessagingProvider {
       if (_initialMessage != null) {
         onMessage(_initialMessage);
       }
-      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-      var file;
-      var bigPictureStyleInformation;
-      List<IOSNotificationAttachment> newattachments=[];
-      String? avatar;
 
-      FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-        print("onmessage.listen ontap");
-
-        RemoteNotification? notification = message.notification;
-        // AndroidNotification? android = message.notification?.android;
-        AppleNotification? appleNot = message.notification?.apple;
-        avatar = appleNot!.imageUrl;
-
-        print(message.data);
-        print("getinitiaonMessage.listen ontap");
-
-        if (Platform.isAndroid) {
-
-        } else if (Platform.isIOS) {
-          if (avatar != null && avatar!.isNotEmpty) {
-            final extension = p.extension(avatar!);
-            final picturePath = await saveImage(Image.network(avatar!),extension);
-            newattachments.add(IOSNotificationAttachment('${picturePath}'));
-          }
-        }
-
-        if (notification != null) {
-          print("message from ${message.notification!.body}");
-          print("message from ${message.notification!.title}");
-          print("message from ${message.data}");
-
-          var platformChannelSpecificsAndroid = new AndroidNotificationDetails(
-              'your_channel_id', 'your channel name',
-              channelDescription: 'your channel description',
-              icon: 'logo_blue',
-              color: Colors.indigo,
-              playSound: false,
-              enableVibration: false,
-              importance: Importance.high,
-              styleInformation: avatar != null && avatar!.isNotEmpty
-                  ? bigPictureStyleInformation
-                  : null,
-              largeIcon: avatar != null && avatar!.isNotEmpty ? file : null,
-              priority: Priority.high);
-
-          // @formatter:on
-          var platformChannelSpecificsIos =
-          new IOSNotificationDetails(
-              presentSound: true,
-              presentAlert: true,
-              presentBadge: true,
-              attachments: newattachments
-          );
-          var platformChannelSpecifics = new NotificationDetails(
-              android: platformChannelSpecificsAndroid,
-              iOS: platformChannelSpecificsIos);
-
-          flutterLocalNotificationsPlugin.show(
-            1,
-            notification.title,
-            notification.body,
-            platformChannelSpecifics,
-          );
-        }
-      });
+      FirebaseMessaging.onMessage.listen(onMessage);
     } catch (e) {
       print(e);
       print('Error!!!: Receiving message from notification');
@@ -246,25 +108,4 @@ class FirebaseMessagingProvider {
       print('Error!!!: Resetting firebase messaging');
     }
   }
-}
-
-Future<String> saveImage(Image image,String fileExtention) {
-  final completer = Completer<String>();
-
-  image.image.resolve(ImageConfiguration()).addListener(
-      ImageStreamListener((ImageInfo imageInfo, bool synchronousCall) async {
-        final byteData =
-        await imageInfo.image.toByteData(format: ImageByteFormat.png);
-        final pngBytes = byteData!.buffer.asUint8List();
-
-        final fileName = pngBytes.hashCode;
-        final directory = await getApplicationDocumentsDirectory();
-        final filePath = '${directory.path}/$fileName$fileExtention';
-        final file = File(filePath);
-        await file.writeAsBytes(pngBytes);
-
-        completer.complete(filePath);
-      }));
-
-  return completer.future;
 }
